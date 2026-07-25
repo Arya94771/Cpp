03ee18820dc5dc330f364785cc9b2064a0c62cbc
sri_arya16 <aryasrivastav9270@gmail.com>
2026-07-26 03:38:38 +0530

    Learned about overflow case and operators order.

---
+++ overflow-calculation.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  // int s=3/2;
+  // cout<<s;
+  int l=100000;
+  int c=100000;
+  // int d=l*c; //overflow because of max range for int is 10^9
+  // cout<<d;
+  long long int k=l*1LL*c;
+  cout<<k;
+}
