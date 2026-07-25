af5cd2dcb2d75fc2822e0465dbf8a55ef480b3c0
sri_arya16 <aryasrivastav9270@gmail.com>
2026-07-26 02:05:07 +0530

    Learned basics of cpp

---
+++ first.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   //char,int,bool,double
+   //int a=3;
+   // char c='Y';
+   // double b=3.5;
+   // bool d=true;
+   // bool e=false;
+   //cout <<"Hello world" <<" My name is"<<"Arya Srivastav"<<endl;
+   //cout <<a << " "<< c<< " "<< b<<" "<<d<<" "<<e<<endl;}
+   //cout<< 10%3<<endl;
+   // a=a+1;
+   // cout<<a<<endl;
+   // cout<<a++<<endl;
+   // cout<<a--<<endl;
+   // cout<<--a<<endl;
+   char c='b';
+   cout<<(int)c;
+   int x;
+   cin>>x;
+   cout<<x;
+   
+
+}
