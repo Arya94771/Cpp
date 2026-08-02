aabe47e2fba796b17680cb2dbc84b38f71f520f4
sri_arya16 <aryasrivastav9270@gmail.com>
2026-08-03 04:29:13 +0530

    Practiced string(function-getline(),push_back()).

---
+++ string-getline-bignum.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+// int main(){
+//   string str,str1;
+//   getline(cin,str);
+//   getline(cin,str1);
+//   cout<<str<<endl;
+//   cout<<str1<<endl;
+//   return 0;
+// }
+
+
+// int main(){
+//   string s="Hello World";
+//   string a;
+//   for(int i=s.size()-1;i>=0;--i){
+//     a.push_back(s[i]); 
+
+//   }
+//   cout<<a<<endl;
+// }
+
+int main(){
+  string s="3242424244214242124214242424";
+  int l=s[s.size()-1]-'0';
+  cout<<l<<endl;
+}
